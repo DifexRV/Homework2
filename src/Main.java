2b6642ed2c590d508131fedf8dd470b8624342d2
DifexRV <roOmkin@gmail.com>
2022-09-20 21:37:56 +0700

    Ветка к блоку 3/3 ДЗ. Разграничил задачи + Исправил на первоначальное значение своей последней задачи.

--- src/Main.java
+++ src/Main.java
@@ -1,18 +1,22 @@
 public class Main {
     public static void main(String[] args) {
+       //Задача 6
+       System.out.println("Задача 6" );
    var boxer1 = 78.2;
    var boxer2 = 82.7;
    var totalWeight = boxer1 + boxer2;
    System.out.println("Общий вес двух боксеров " + totalWeight + " кг!");
 
+   //Задача 7
+       System.out.println("Задача 7");
    var difference1 = boxer2 - boxer1;
    System.out.println("Способ 1. Разница в весе составляет " + difference1 + " кг!");
-
    var difference2 = (boxer2 % boxer1);
    System.out.println("Способ 2. Разница в весе составляет " + difference2 + " кг!");
 
 
-
+   //Задача 8
+       System.out.println("Задача 8");
    var totalWH = 640;
    var empWH = 8;
    var totalNumbEmp = totalWH / empWH;
